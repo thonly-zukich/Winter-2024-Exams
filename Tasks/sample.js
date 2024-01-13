fa078e68aaf203fd33bce5f6e9470cb1d1eb928d
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2024-01-13 22:02:19 +0200

    Update sample.js
    Splitted the logic into separate steps to improve readability.

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,6 +1,9 @@
 const getRandomElement = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  const randomElement = arr[randomIndex];
+
+  return randomElement;
 };
 
 module.exports = getRandomElement;
