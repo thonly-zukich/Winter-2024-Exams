b616e4d323af2db52f28dc4f21f61bde2def2b86
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2024-01-13 21:46:18 +0200

    Update password.js
    Changed Index to index for consistent naming conventions.
    Changed the concatenation to use the += operator for better readability.
    Followed the two-space indentation and added an empty line between semantic blocks.

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,11 +1,13 @@
 const generatePassword = (alphabet, length) => {
   const alphabetLength = alphabet.length;
+
   let password = '';
+
   for (let i = 0; i < length; i++) {
-    const Index = Math.floor(Math.random() * alphabetLength);
-    password = password + alphabet[Index];
+    const index = Math.floor(Math.random() * alphabetLength);
+
+    password += alphabet[index];
   }
+
   return password;
 };
-
-module.exports = generatePassword;
