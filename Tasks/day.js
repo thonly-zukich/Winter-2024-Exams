6ebb61f8cebe45cd313da0673783823170b26f30
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2024-01-13 23:04:36 +0200

    Update day.js
    Used toLowerCase() to make the comparison case-insensitive.
    Removed the unnecessary else block since it returns in the if block.

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,10 +1,12 @@
 const getDayNumber = (dayString) => {
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
   for (let i = 0; i < daysOfWeek.length; i++) {
     if (dayString.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
   }
+
   return -1;
 };
 
