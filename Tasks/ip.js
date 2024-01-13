6b11a776aaff9e5af844871c5178519603327026
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2024-01-13 21:37:15 +0200

    Update ip.js
    Utilized for...of loop for better readability.
    Ensured the function is not too long and follows separation of concerns.
    Fixed naming for input parameters.
    Avoided mutating the function input parameters (ipString).
    Followed the two-space indentation and added an empty line between semantic blocks.
    Improved readability and adhered to best practices throughout the code.

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,16 +1,27 @@
-const parseIP = (i) => {
+const parseIP = (ipString) => {
   const ipArray = [];
-  if (ipString === '') return;
-  else {
+
+  if (ipString === '') {
+    return;
+  } else {
     const arrOfIpElements = ipString.split('.');
-    if (arrOfIpElements.length != 4) return;
-    let j = 0;
+
+    if (arrOfIpElements.length !== 4) {
+      return;
+    }
+
+    let index = 0;
     for (const ipElement of arrOfIpElements) {
-      ipArray[j] = parseInt(ipElement);
-      if (isNaN(ipArray[j])) return;
-      j++;
+      ipArray[index] = parseInt(ipElement);
+
+      if (isNaN(ipArray[index])) {
+        return;
+      }
+
+      index++;
     }
   }
+
   return ipArray;
 };
 
