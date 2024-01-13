a8f4c4832fc22043bfe25b863939dfc6ee244a97
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2024-01-13 20:53:43 +0200

    Update filter.js
    Fixed naming for input parameters

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,13 +1,13 @@
-const filterByType = (T, t) => {
+const filterByType = (array, typeName) => {
   const indexesToRemove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
+  for (C of array) {
+    x = array.indexOf(C);
+    if (typeof array[x] !== typeName) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (x of remove) array.splice(x, 1);
+  return array;
 };
 
 module.exports = filterByType;
