b58c10bbc200f1f716e6a220f7f4b86f3f66f4bc
thonly-zukich <142508205+thonly-zukich@users.noreply.github.com>
2024-01-13 22:58:49 +0200

    Update day.js
    Used const for variables and let only when necessary.
    Removed unnecessary comments.

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,10 +1,8 @@
-// Get day number
+const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
+const _parse_day_ = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
+  for (let i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
